d051f4c9bbc5bb32cbe2f5338f9cc84bd06af300
bunnyphoon <znzlspt17@gmail.com>
2024-05-22 16:07:34 +0900

    인터페이스 이름 변경

--- netcore/src/main/java/com/bunnyphoon/netcore/command/CommandService.java
+++ netcore/src/main/java/com/bunnyphoon/netcore/command/CommandService.java
@@ -9,7 +9,7 @@ import java.util.Map;
 /**
  * 사용될 프로젝트에서 구현해야될 커맨드 서비스의 원형입니다.
  */
-public interface ICommandService {
+public interface CommandService {
     public abstract void setTable(Map<Integer, Command> table);
 
     public abstract void addCommand(int key, Command command);
